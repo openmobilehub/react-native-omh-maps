535a8e688edb9f4701151b00feda3bc3a2f87959
artus9033 <artus9033@gmail.com>
2024-03-20 15:37:00 +0100

    chore: updated commitlint config to require Signed-off-by
    Signed-off-by: artus9033 <artus9033@gmail.com>

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'signed-off-by': [2, 'always', 'Signed-off-by:'],
+  },
 };
 
 export default config;
