7d735015c0b373d23680a9a53ef4b83df8f2fe58
artus9033 <artus9033@gmail.com>
2024-03-22 11:03:57 +0100

    chore: docs template setup
    Signed-off-by: artus9033 <artus9033@gmail.com>

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,130 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/openmobilehub/react-native-omh-maps/tree/main/docs/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/openmobilehub/react-native-omh-maps',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Guides & documentation',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'API Reference',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/openmobilehub',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/openmobilehub/react-native-omh-maps',
+            },
+            {
+              label: 'Android OMH Maps GitHub',
+              href: 'https://github.com/openmobilehub/android-omh-maps',
+            },
+            {
+              label: 'OMH Project Homepage',
+              href: 'https://openmobilehub.com',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} OpenMobileHub. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -12,12 +12,18 @@ const LINKING_ERROR =
   '- You are not using Expo Go\n';
 
 type RnOmhMapsCoreProps = {
+  /** Test color property */
   color: string;
+  /** Test style property */
   style: ViewStyle;
 };
 
 const ComponentName = 'RnOmhMapsCoreView';
 
+/**
+ * The root map view component. Actual implementation is picked based on the platform capabilities (GMS or non-GMS)
+ * and availability of installed providers (`@omh/react-native-maps-plugin-*`).
+ */
 export const RnOmhMapsCoreView =
   UIManager.getViewManagerConfig(ComponentName) != null
     ? requireNativeComponent<RnOmhMapsCoreProps>(ComponentName)
