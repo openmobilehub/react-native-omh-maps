ace8a47a2aab7dfad64b4974d9ffbe2c545cd98f
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-05-21 10:37:26 +0200

    fix: fix events on iOS maps
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

--- sample-app/src/screens/demos/LocationSharingScreen.tsx
+++ sample-app/src/screens/demos/LocationSharingScreen.tsx
@@ -91,10 +91,6 @@ export const LocationSharingScreen = ({ navigation }: Props) => {
   };
 
   const handleMapLoaded = () => {
-    enableLocation();
-  };
-
-  const enableLocation = () => {
     requestLocationPermission();
   };
 
