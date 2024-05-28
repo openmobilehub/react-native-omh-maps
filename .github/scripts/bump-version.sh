c6be850bac1d85b1eafa8d5af05b5ee03afd01ab
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-05-28 12:10:34 +0200

    chore: configure lerna
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

---
+++ .github/scripts/bump-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+yarn lerna version --no-push --no-git-tag-version --no-changelog --no-private
+
+PACKAGE_VERSION=$(jq -r .version < lerna.json)
+
+echo "Creating a release branch..."
+git checkout -b "v$PACKAGE_VERSION"
+git add -A
+git commit -s -S -m "chore: release v$PACKAGE_VERSION"
+git push origin "v$PACKAGE_VERSION"
+
+echo ""
+echo "Release branch created"
+echo "Please create a pull request to merge the release branch into main"
+echo "https://github.com/openmobilehub/react-native-omh-auth/pull/new/v${PACKAGE_VERSION}"
