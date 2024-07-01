d9bdf186a4d145e58430e20862a80423f1ad0066
Andrei Zgîrvaci <zgirvaciandrei@gmail.com>
2024-07-01 19:01:31 +0300

    ci: create release pipeline (#99)
    Closes: #65
    
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- .github/scripts/check-version.sh
+++ .github/scripts/check-version.sh
@@ -1,11 +1,14 @@
 PACKAGE_VERSION=$(jq -r .version < lerna.json)
 git fetch --tags
 
+echo "PACKAGE_VERSION=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
+
 if git tag | grep -x "v${PACKAGE_VERSION}" > /dev/null; then
-  echo "Version ${PACKAGE_VERSION} already exists, no new tag created."
-  echo "version_changed=false" >> $GITHUB_ENV
+  echo "Version: ${PACKAGE_VERSION} already exists. Skipping..."
+
+  echo "VERSION_CHANGED=false" >> $GITHUB_OUTPUT
 else
   echo "New version detected: ${PACKAGE_VERSION}"
-  echo "version_changed=true" >> $GITHUB_ENV
-  echo "PACKAGE_VERSION=${PACKAGE_VERSION}" >> $GITHUB_ENV
+
+  echo "VERSION_CHANGED=true" >> $GITHUB_OUTPUT
 fi
