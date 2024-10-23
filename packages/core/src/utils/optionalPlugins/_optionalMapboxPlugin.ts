be5d532f8cbe0819f65a346d3226c11ebc2a18e5
Adam Trzciński <trzcinski.adam@gmail.com>
2024-10-23 11:31:40 +0200

    fix: optional plugins exports
    Signed-off-by: Adam Trzciński <trzcinski.adam@gmail.com>

--- packages/core/src/utils/optionalPlugins/_optionalMapboxPlugin.ts
+++ packages/core/src/utils/optionalPlugins/_optionalMapboxPlugin.ts
@@ -1,1 +1,11 @@
-export default require('@openmobilehub/maps-plugin-mapbox');
+let mapboxPlugin;
+
+try {
+  mapboxPlugin = require('@openmobilehub/maps-plugin-mapbox');
+} catch (error) {
+  // Fallback or default behavior if Mapbox plugin is not installed
+  console.warn('@openmobilehub/maps-plugin-mapbox is not installed');
+  mapboxPlugin = null;
+}
+
+export default mapboxPlugin;
