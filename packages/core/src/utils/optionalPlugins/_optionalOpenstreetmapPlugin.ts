53c5c379a73c9caf48d5ff1280ceae7af2ec6767
Adam Trzciński <trzcinski.adam@gmail.com>
2024-10-23 16:56:11 +0200

    fix: optional plugins exports (#120)
    Signed-off-by: Adam Trzciński <trzcinski.adam@gmail.com>

--- packages/core/src/utils/optionalPlugins/_optionalOpenstreetmapPlugin.ts
+++ packages/core/src/utils/optionalPlugins/_optionalOpenstreetmapPlugin.ts
@@ -1,1 +1,10 @@
-export default require('@openmobilehub/maps-plugin-openstreetmap');
+let openStreetMapPlugin;
+
+try {
+  openStreetMapPlugin = require('@openmobilehub/maps-plugin-openstreetmap');
+} catch (error) {
+  console.warn('@openmobilehub/maps-plugin-openstreetmap is not installed');
+  openStreetMapPlugin = null;
+}
+
+export default openStreetMapPlugin;
